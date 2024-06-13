c706aecfd8b297f26c5bf96d805f92a5e892f11d
LerautB <bastien.leraut@free.fr>
2024-06-13 17:09:30 +0200

    update requirements.txt, creation Conversation de groupe, update gitignore

--- app_chat/urls.py
+++ app_chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ProfileView, FriendListView, AddFriendView, ManageFriendRequestView, SearchUsersView,RemoveFriendView,ChatView
+from .views import ProfileView, FriendListView, AddFriendView, ManageFriendRequestView, SearchUsersView,RemoveFriendView,ChatView,create_conversation
 
 urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
@@ -10,6 +10,6 @@ urlpatterns = [
     path('manage_friend_request/<int:request_id>/', ManageFriendRequestView.as_view(), name='manage_friend_request'),
     path('search_users/', SearchUsersView.as_view(), name='search_users'),
     path('remove_friend/<int:friend_id>/', RemoveFriendView.as_view(), name='remove_friend'),
-    path('chat/<int:conversation_id>/', ChatView.as_view(), name='chatroom')
-
+    path('chat/<int:conversation_id>/', ChatView.as_view(), name='chatroom'),
+    path('create_conversation/', create_conversation, name='create_conversation'),
 ]
