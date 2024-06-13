56f2c7f2cde0e0d70f59f7e9848cf428c6e6b97e
LerautB <bastien.leraut@free.fr>
2024-06-13 10:18:39 +0200

    Finalisation Chat + websocket pour conversation en duo + liste d'amis + friend request + add_friend

--- app_chat/consumers.py
+++ app_chat/consumers.py
@@ -1,14 +1,77 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
+# # app_chat/consumers.py
+# import json
+# from channels.generic.websocket import AsyncWebsocketConsumer
+# from channels.db import database_sync_to_async
+# from django.shortcuts import get_object_or_404
+# from .models import Conversation, Message
+
+# class ChatConsumer(AsyncWebsocketConsumer):
+#     async def connect(self):
+#         self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
+#         self.conversation_group_name = f'chat_{self.conversation_id}'
+
+#         # Join room group
+#         await self.channel_layer.group_add(
+#             self.conversation_group_name,
+#             self.channel_name
+#         )
+
+#         await self.accept()
+
+#     async def disconnect(self, close_code):
+#         # Leave room group
+#         await self.channel_layer.group_discard(
+#             self.conversation_group_name,
+#             self.channel_name
+#         )
+
+#     async def receive(self, text_data):
+#         text_data_json = json.loads(text_data)
+#         message = text_data_json['message']
+
+#         # Save the message to the database
+#         conversation = await self.get_conversation(self.conversation_id)
+#         await self.create_message(conversation, self.scope['user'], message)
+
+#         # Send message to room group
+#         await self.channel_layer.group_send(
+#             self.conversation_group_name,
+#             {
+#                 'type': 'chat_message',
+#                 'message': message
+#             }
+#         )
+
+#     async def chat_message(self, event):
+#         message = event['message']
+
+#         # Send message to WebSocket
+#         await self.send(text_data=json.dumps({
+#             'message': message
+#         }))
+
+#     @database_sync_to_async
+#     def get_conversation(self, conversation_id):
+#         return get_object_or_404(Conversation, id=conversation_id)
+
+#     @database_sync_to_async
+#     def create_message(self, conversation, user, message):
+#         return Message.objects.create(conversation=conversation, sender=user, content=message)
+# app_chat/consumers.py
 import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.shortcuts import get_object_or_404
+from .models import Conversation, Message
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = 'chat_room'
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
+        self.conversation_group_name = f'chat_{self.conversation_id}'
 
         # Join room group
         await self.channel_layer.group_add(
-            self.room_group_name,
+            self.conversation_group_name,
             self.channel_name
         )
 
@@ -17,27 +80,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
-            self.room_group_name,
+            self.conversation_group_name,
             self.channel_name
         )
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        sender_id = text_data_json['sender_id']
+
+        # Save the message to the database
+        conversation = await self.get_conversation(self.conversation_id)
+        await self.create_message(conversation, self.scope['user'], message)
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name,
+            self.conversation_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'sender_id': sender_id
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
+        sender_id = event['sender_id']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'sender_id': sender_id
         }))
+
+    @database_sync_to_async
+    def get_conversation(self, conversation_id):
+        return get_object_or_404(Conversation, id=conversation_id)
+
+    @database_sync_to_async
+    def create_message(self, conversation, user, message):
+        return Message.objects.create(conversation=conversation, sender=user, content=message)
--- static/app_chat/js/chat.js
+++ static/app_chat/js/chat.js
@@ -1,64 +1,39 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var chatSocket = new WebSocket(
-        'ws://' + window.location.host + '/ws/chat/');
+    const chatSocket = new WebSocket(
+        'ws://' + window.location.host + '/ws/chat/' + conversationId + '/'
+    );
 
     chatSocket.onmessage = function(e) {
-        var data = JSON.parse(e.data);
-        document.querySelector('#chat-messages').innerHTML += '<div>' + data.message + '</div>';
+        const data = JSON.parse(e.data);
+        const chatLog = document.querySelector('#chat-log');
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.classList.add(data.sender_id === userId ? 'self' : 'other');
+        messageElement.innerHTML = `
+            <div class="message-content">${data.message}</div>
+            <div class="message-timestamp">${new Date().toLocaleTimeString()}</div>
+        `;
+        chatLog.appendChild(messageElement);
     };
 
     chatSocket.onclose = function(e) {
         console.error('Chat socket closed unexpectedly');
     };
 
-    window.sendMessage = function() {
-        var messageInputDom = document.querySelector('#message-input');
-        var message = messageInputDom.value;
+    document.querySelector('#chat-message-input').focus();
+    document.querySelector('#chat-message-input').onkeyup = function(e) {
+        if (e.keyCode === 13) {  // Enter key
+            document.querySelector('#chat-message-submit').click();
+        }
+    };
+
+    document.querySelector('#chat-message-submit').onclick = function(e) {
+        const messageInputDom = document.querySelector('#chat-message-input');
+        const message = messageInputDom.value;
         chatSocket.send(JSON.stringify({
-            'message': message
+            'message': message,
+            'sender_id': userId
         }));
         messageInputDom.value = '';
     };
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Assurez-vous que l'URL de la WebSocket correspond à celle définie dans votre routing.py
-// var chatSocket = new WebSocket(
-//     'ws://' + window.location.host.replace("8000", "8001") + '/ws/chat/'
-// );
-
-// chatSocket.onmessage = function(e) {
-//     var data = JSON.parse(e.data);
-//     document.querySelector('#chat-log').value += (data.message + '\n');
-// };
-
-// chatSocket.onclose = function(e) {
-//     console.error('Chat socket closed unexpectedly');
-// };
-
-// document.querySelector('#chat-message-input').focus();
-// document.querySelector('#chat-message-input').onkeyup = function(e) {
-//     if (e.keyCode === 13) {  // Touche "Enter"
-//         document.querySelector('#chat-message-submit').click();
-//     }
-// };
-
-// document.querySelector('#chat-message-submit').onclick = function(e) {
-//     var messageInputDom = document.querySelector('#chat-message-input');
-//     var message = messageInputDom.value;
-//     chatSocket.send(JSON.stringify({
-//         'message': message
-//     }));
-//     messageInputDom.value = '';
-// };
